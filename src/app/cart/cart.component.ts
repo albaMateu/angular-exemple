2fc7ac8f2aac8726891fddf0ecb8c4a3f7091292
albaMateu <alba.alm.5@gmail.com>
2019-06-11 12:05:45 +0200

    Mostrar los articulos del carrito

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+//utilizar el servicio de carrito para mostrar los productos en el carrito.
+import { CartService } from '../cart.service';
 
 @Component({
   selector: 'app-cart',
@@ -6,8 +8,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cart.component.css']
 })
 export class CartComponent implements OnInit {
-
-  constructor() { }
+  //almacenar los productos en el carrito
+  items;
+  constructor(
+    private cartService: CartService
+  ) {
+    this.items= cartService.getItemsCart();
+   }
 
   ngOnInit() {
   }
--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
 import { products } from '../products';
+//utilizará el servicio de carrito para agregar el producto actual al carrito
+import { CartService } from '../cart.service';
 
 @Component({
   selector: 'app-product-details',
@@ -14,12 +16,18 @@ export class ProductDetailsComponent implements OnInit {
   constructor(
     //Contiene información sobre la ruta, sus parámetros y datos adicionales asociados con la ruta
     private route: ActivatedRoute,
+    private cartService: CartService
   ) { }
 
   ngOnInit() {
     this.route.paramMap.subscribe(params => {
       this.product =products[+params.get('productId')];
     });
+  }
+
+  addToCart(product){
+    window.alert("Producto añadido al carrito");
+    this.cartService.addToCart(product);
   }
 
 }
